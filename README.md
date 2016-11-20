926a64c15c7950f3ef8f81aa52e4c73178b2850d
mo <feng_zilong@163.com>
2016-11-20 23:23:19 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # flow
+
+```bash
+$ flow check
+```
+
+or with colors
+
+```bash
+$ flow check --color always
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+// @flow
+
+var str: number = 'hello world';
+
+function A(): void {
+	console.log( B );
+
+	return 1;
+}
+
+A();
